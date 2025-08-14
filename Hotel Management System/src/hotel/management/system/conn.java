072b781463e1efd5a13cb36b12d52ef3be759ced
Gantaudaykiran <gantaudaykiran123@gmail.com>
2025-08-14 22:41:42 +0530

    second commit

--- Hotel Management System/src/hotel/management/system/conn.java
+++ Hotel Management System/src/hotel/management/system/conn.java
@@ -14,16 +14,14 @@ public class conn{
     Connection c;
     Statement s;
     public conn(){  
-        try{  
-            Class.forName("com.mysql.jdbc.Driver");  
-            c =DriverManager.getConnection("jdbc:mysql:///hms","root",""); 
-            
-            s =c.createStatement();  
-            
-           
-        }catch(Exception e){ 
-            System.out.println(e);
-        }  
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///hotelmanagementsystem", "root", "uday123");
+            s = c.createStatement();
+            } 
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }  
 }  
 
